8ea33c791f00bfedb05c395750e5650e232dfc9e
kristinakostiuk <krictinamalinovska2003@gmail.com>
2024-05-22 18:15:52 +0300

    hw

--- src/main.js
+++ src/main.js
@@ -8,37 +8,34 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css"; 
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-
-
 import { fetchPhotosByPixabay } from "./js/pixabay-api";
 import { createGallery } from "./js/render-functions";
+
+function showMessage(message, type = 'error') {
+    iziToast[type]({
+        message: message,
+        position: 'topRight',
+        maxWidth: '300px'
+    });
+}
 
 function onSearchFormSubmit(event) {
     event.preventDefault();
     const searchQuery = event.target.elements.searchKeywords.value.trim();
     if (searchQuery === '') {
+        showMessage('Sorry, there are no images matching your search query. Please try again!');
         refs.galleryList.innerHTML = '';
-        event.target.reset()
-        iziToast.error({
-            message: 'Sorry, there are no images matching your search query. Please try again!',
-            position: 'topRight',
-            maxWidth: '300px'
-        })
-        return
+        return;
     }
     refs.loader.classList.remove('is-hidden');
 
     fetchPhotosByPixabay(searchQuery)
         .then(imagesData => {
             if (imagesData.hits.length === 0) {
-                iziToast.error({
-                    message: 'Sorry, there are no images matching your search query. Please try again!',
-                    position: 'topRight',
-                    maxWidth: '300px'
-                })
+                showMessage('Sorry, there are no images matching your search query. Please try again!');
+                return;
             }
             return imagesData;
-            refs.galleryList.innerHTML = '';
         })
         .then(imagesData => {
             const markup = createGallery(imagesData.hits);
@@ -47,17 +44,11 @@ function onSearchFormSubmit(event) {
         })
         .catch(error => {
             console.error('Error:', error);
-            iziToast.error({
-                message: 'An error occurred while fetching images. Please try again later.',
-                position: 'topRight',
-                maxWidth: '300px'
-            });
         })
         .finally(() => {
-            event.target.reset()
+            event.target.reset();
             refs.loader.classList.add('is-hidden');
-        })
+        });
 }
 
 refs.form.addEventListener('submit', onSearchFormSubmit);
-
